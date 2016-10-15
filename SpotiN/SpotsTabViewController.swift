ae10444f9a9313b7a7380d672246dc9c9a87538c
Gabriel Nieves Ponce <gaby@Gabriels-MacBook-Pro.local>
2016-10-14 21:28:43 -0400

    Created initial realese of the app with minimalistic components and mock UI. The view controllers that were created go as follows: authentication VC, mapVC, SpotsInfoVC.

---
+++ SpotiN/SpotsTabViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SpotsTabViewController.swift
+//  SpotiN
+//
+//  Created by Gabriel Nieves Ponce on 10/14/16.
+//  Copyright © 2016 BTEC. All rights reserved.
+//
+
+import UIKit
+
+class SpotsTabViewController: UITabBarController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
