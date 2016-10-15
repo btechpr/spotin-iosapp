04a7c3ebda270d3acce08978eba5bae65a7025e2
Gabriel Nieves <gabrielnieves18@gmail.com>
2016-10-14 20:53:05 -0400

    Initial Commit

---
+++ SpotiN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotiN
+//
+//  Created by Gabriel Nieves Ponce on 10/14/16.
+//  Copyright © 2016 BTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
